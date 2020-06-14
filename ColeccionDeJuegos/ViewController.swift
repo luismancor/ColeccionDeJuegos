81be8aa536db7919841ffc4538012de31408c795
luismancor <juan.mancilla.c@tecsup.edu.pe>
2020-06-14 10:53:50 -0700

    no message

---
+++ ColeccionDeJuegos/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ColeccionDeJuegos
+//
+//  Created by Juan Mancilla on 6/13/20.
+//  Copyright © 2020 Juan Mancilla. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
